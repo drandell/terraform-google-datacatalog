8be196ba95d16b2191d18cc996282d46988386f7
Daniel Randell <danielrandell@outlook.com>
2022-04-12 20:33:43 +0100

    working taxonomy, max levels supported

--- examples/data_catalog_taxonomy_policy_tags/terraform.tfvars
+++ examples/data_catalog_taxonomy_policy_tags/terraform.tfvars
@@ -5,8 +5,38 @@ taxonomy_policy_tags = [
   {
     id           = "my_taxonomy_id"
     display_name = "My taxonomy display name"
+    activated_policy_types = ["FINE_GRAINED_ACCESS_CONTROL"]
     policy_tags = [{
-      display_name = "My Test Tag"
+      id = "my_first_tag"
+      display_name = "My First Tag"
+      level_one = [
+        {
+          id = "my_level_one_tag"
+          display_name = "Lvl One Tag"
+          parent_tag = "my_first_tag"
+        },
+      ]
+      level_two = [
+        {
+          id = "my_level_two_tag"
+          display_name = "Lvl Two Tag"
+          parent_tag = "my_level_one_tag"
+        }
+      ]
+      level_three = [
+        {
+          id = "my_level_three_tag"
+          display_name = "Lvl Three Tag"
+          parent_tag = "my_level_two_tag"
+        }
+      ]
+      level_four = [
+        {
+          id = "my_level_four_tag"
+          display_name = "Lvl Four Tag"
+          parent_tag = "my_level_three_tag"
+        }
+      ]
     }]
   }
 ]
--- modules/taxonomy-policy-tags/variables.tf
+++ modules/taxonomy-policy-tags/variables.tf
@@ -5,26 +5,39 @@ variable "taxonomy_policy_tags" {
     description            = optional(string)
     activated_policy_types = optional(list(string))
     policy_tags = optional(list(object({
+      id = string
       display_name = string
       description  = optional(string)
-      level_two = optional(object({
+      level_one = optional(list(object({
+        id = string
         display_name = string
+        parent_tag   = string
         description  = optional(string)
-      })),
-      level_three = optional(object({
+      }))),
+      level_two = optional(list(object({
+        id = string
         display_name = string
+        parent_tag   = string
         description  = optional(string)
-      })),
-      level_four = optional(object({
+      }))),
+      level_three = optional(list(object({
+        id = string
         display_name = string
+        parent_tag   = string
         description  = optional(string)
-      }))
+      }))),
+      level_four = optional(list(object({
+        id = string
+        display_name = string
+        parent_tag   = string
+        description  = optional(string)
+      })))
     })))
   }))
   description = "(Required) A list of taxonomy and policy tags"
 
   validation {
-    condition     = alltrue([for taxonomy in var.taxonomy_policy_tags : taxonomy["activated_policy_types"] == null ? true : contains(["POLICY_TYPE_UNSPECIFIED", "FINE_GRAINED_ACCESS_CONTROL"], taxonomy["activated_policy_types"])])
+    condition     = alltrue([ for taxonomy in var.taxonomy_policy_tags : alltrue([ for policy_type in lookup(taxonomy, "activated_policy_types", "POLICY_TYPE_UNSPECIFIED") : contains(["POLICY_TYPE_UNSPECIFIED", "FINE_GRAINED_ACCESS_CONTROL"], policy_type) ]) ])
     error_message = "Supported policy types are 'FINE_GRAINED_ACCESS_CONTROL' and 'POLICY_TYPE_UNSPECIFIED'."
   }
 }
